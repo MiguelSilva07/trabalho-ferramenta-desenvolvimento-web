986daed93e0df1e43a06aa7bf391138dd1049a9a
Miguel Silva <132858476+MiguelSilva07@users.noreply.github.com>
2023-05-21 17:53:47 -0400

    Update app.component.ts

--- dev-web/src/app/app.component.ts
+++ dev-web/src/app/app.component.ts
@@ -10,11 +10,11 @@ export class AppComponent {
   title = 'projetoUninter';
   data: Array<any>;constructor(){
     this.data = [
-      {Name: 'MIGUEL MAGALHÃES DA SILVA', RU: ' 3576177', CURSOS: ' Data Science', DNS: '25.01.1999'},
-      {Name: 'MAGALHÃES', RU: ' 1234567', CURSOS: ' Artificial Intelligence', DNS: '23.12.1997'},
-      {Name: 'SILVA', RU: ' 7654321', CURSOS: ' Machine Learning', DNS: '25.01.1999'},
-      {Name: 'MARQUES', RU: ' 3576178', CURSOS: ' Deep Learning', DNS: '25.01.1972'},
-      {Name: 'PAIVA', RU: ' 3576155', CURSOS: ' Internet of Things (IoT)', DNS: '25.01.1974'}
+      {Name: 'MIGUEL MAGALHÃES DA SILVA', RU: ' 3576177', CURSOS: ' Data Science', DNS: '99/99/9999'},
+      {Name: 'MAGALHÃES', RU: ' 1234567', CURSOS: ' Artificial Intelligence', DNS: '98/98/9998'},
+      {Name: 'SILVA', RU: ' 7654321', CURSOS: ' Machine Learning', DNS: '97/99/9997'},
+      {Name: 'MARQUES', RU: ' 3576178', CURSOS: ' Deep Learning', DNS: '96/99/9996'},
+      {Name: 'PAIVA', RU: ' 3576155', CURSOS: ' Internet of Things (IoT)', DNS: '95/99/9995'}
     ];
 
   }
