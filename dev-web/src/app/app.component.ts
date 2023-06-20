cf26c6e786900ae34e2c1958adf86a0d66513a3b
Miguel Silva <132858476+MiguelSilva07@users.noreply.github.com>
2023-06-20 11:40:39 -0400

    Update app.component.ts

--- dev-web/src/app/app.component.ts
+++ dev-web/src/app/app.component.ts
@@ -10,11 +10,11 @@ export class AppComponent {
   title = 'projetoUninter';
   data: Array<any>;constructor(){
     this.data = [
-      {Name: '1', RU: ' 2556354', CURSOS: ' Data Science', DNS: '99/99/9999'},
-      {Name: '2', RU: ' 1234567', CURSOS: ' Artificial Intelligence', DNS: '98/98/9998'},
-      {Name: '3', RU: ' 7654321', CURSOS: ' Machine Learning', DNS: '97/99/9997'},
-      {Name: '4', RU: ' 3576178', CURSOS: ' Deep Learning', DNS: '96/99/9996'},
-      {Name: '5', RU: ' 3576155', CURSOS: ' Internet of Things (IoT)', DNS: '95/99/9995'}
+      {Name: '1', RU: ' 1556354', CURSOS: ' Data Science', DNS: '99/99/9999'},
+      {Name: '2', RU: ' 2234567', CURSOS: ' Artificial Intelligence', DNS: '98/98/9998'},
+      {Name: '3', RU: ' 3654321', CURSOS: ' Machine Learning', DNS: '97/99/9997'},
+      {Name: '4', RU: ' 4576178', CURSOS: ' Deep Learning', DNS: '96/99/9996'},
+      {Name: '5', RU: ' 5576155', CURSOS: ' Internet of Things (IoT)', DNS: '95/99/9995'}
     ];
 
   }
